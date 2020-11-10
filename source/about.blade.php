2da70993c8fc1555cf4eca778c1af08f42e0e1bd
Phil Stephens <phil@Phils-MBP-2.lan>
2020-11-10 10:29:01 +1000

    first commit

---
+++ source/about.blade.php
@@ -0,0 +1,17 @@
+---
+title: About
+description: A little bit about the site
+---
+@extends('_layouts.master')
+
+@section('body')
+    <h1>About</h1>
+
+    <img src="/assets/img/about.png"
+        alt="About image"
+        class="flex rounded-full h-64 w-64 bg-contain mx-auto md:float-right my-6 md:ml-10">
+
+    <p class="mb-6">Hi, I'm an aspiring marathon mountain biker and creative software developer.</p>
+
+    <p class="mb-6">I'm an experienced technical lead and web application developer, with a particular interest in the strategy and architecture of enterprise systems, and the opportunities that technology can afford businesses.  An avid follower of the latest technical trends and their commercial opportunities, and over a decade of experience architecting and building solutions that support and drive businesses.</p>
+@endsection
