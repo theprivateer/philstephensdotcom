6d877c477fe035f8b0780087dd211bf073e3b3cd
Phil Stephens <phil@Phils-MBP-2.lan>
2020-11-12 19:44:55 +1000

    Resize large images on build

---
+++ listeners/ResizeImages.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Listeners;
+
+use TightenCo\Jigsaw\Jigsaw;
+use Intervention\Image\ImageManagerStatic as Image;
+
+class ResizeImages
+{
+	public function handle(Jigsaw $jigsaw)
+	{
+		// Cycle through all images in $jigsaw->getDestinationPath() . '/assets/img'
+		// Construct the iterator
+		$it = new \RecursiveDirectoryIterator($jigsaw->getDestinationPath() . '/assets/img');
+		
+		// Loop through files
+		foreach(new \RecursiveIteratorIterator($it) as $file) {
+			if(@is_array(getimagesize($file))){
+				$img = Image::make($file);
+				
+				if($img->width() > 850)
+				{
+					$img->widen(850, function ($constraint) {
+						$constraint->upsize();
+					});
+				
+					// Save image...
+					$img->save($file->getPathname());
+						
+				}
+			}
+			
+		}
+	}
+}
