5b9e9d950bae532ddd981b5f8a1c24198b1610c3
Phil Stephens <phil@admin-vpn-user-10-240-48-181.uniaccess.unimelb.edu.au>
2021-03-01 14:06:41 +1000

    Switch up the contact page

--- source/contact.blade.php
+++ source/contact.blade.php
@@ -8,11 +8,7 @@ description: Get in touch with me
 <h1 class="font-serif">Contact</h1>
 
 <p class="mb-6">
-    I'm not a massive social media user but you can DM me on <a href="https://www.instagram.com/wattsandhops/">Instagram</a> or <a href="https://www.linkedin.com/in/phil-stephens/">LinkedIn</a>.
-</p>
-
-<p class="mb-6">
-    Alternatively, just drop me an email at <a href="mailto:phil.stephens.bne@gmail.com">phil.stephens.bne@gmail.com</a> and I'll get back to you as soon as possible!
+    Want to get in touch? Just drop me an email at <a href="mailto:phils@hey.com">phils@hey.com</a> and I'll get back to you as soon as possible!
 </p>
 
 @stop
