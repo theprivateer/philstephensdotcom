1862f761e251c9c2034b31513cb1af0836c64cc5
Phil Stephens <phils@hey.com>
2023-07-27 06:35:39 +1000

    Update 'home' link

--- source/_partials/up-next.blade.php
+++ source/_partials/up-next.blade.php
@@ -1,8 +1,3 @@
-{{--<section class="subscribe">--}}
-{{--    <h3>Where do you go from here?</h3>--}}
-{{--    <p>Follow via <a href="/rss.xml">RSS</a> or <a href="https://buttondown.email/philstephens" target="_blank">Email</a>. Thoughts? Comments? <a href="/contact">Get in touch</a>.</p>--}}
-{{--</section>--}}
-
 @if ($previous = $page->getPrevious())
     <div class="pill">
         <a href="{{ $previous->getUrl() }}" title="Newer Post: {{ $previous->title }}">
